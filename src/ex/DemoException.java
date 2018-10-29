47210b924d30af4fb142ae3527e83d77622e2147
yobastono <weissjoscha1@gmail.com>
2018-10-29 19:30:38 +0100

    IOException

--- src/ex/DemoException.java
+++ src/ex/DemoException.java
@@ -5,6 +5,10 @@
  */
 package ex;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author root
@@ -17,17 +21,28 @@ public class DemoException {
     public static void main(String[] args) {
         
         int i, j, k = 0;
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
         int a[] = new int[4];
         i = 8;
         j = 2;
         try{
+            
+        j = Integer.parseInt(br.readLine());
         k = i / j;
-        for (int c = 0; c <= 4; c++){
+        
+        for (int c = 0; c < 4; c++){
             a[c] = c + 1;
         }
         for ( int value : a){
             System.out.println(value);
         }
+  
+        }
+        catch(IOException e){
+            System.out.println("Some IO Error");
         }
         catch(ArithmeticException e){
             System.out.println("cannot divide by zero " + e);
@@ -40,5 +55,4 @@ public class DemoException {
         }
         System.out.println(k);
     }
-    // Comment for testing purpose
 }
